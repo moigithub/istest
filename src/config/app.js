720437190cb24bbc77e1f4931b9d3a9f72ea66f4
moigithub <mcarlosman@gmail.com>
2018-08-23 17:58:54 -0500

    changed api to new graphql server

--- src/config/app.js
+++ src/config/app.js
@@ -1,2 +1,5 @@
-export const SERVER = "http://192.168.1.155:8000"
-export const URI = `${SERVER}/graphql/`
+//export const SERVER = "http://192.168.1.155:8000"
+
+export const SERVER = "https://eu1.prisma.sh/sergio-melendez-f22980/database/dev";
+//export const URI = `${SERVER}/graphql/`
+export const URI = `${SERVER}/`
